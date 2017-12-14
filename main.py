af7c387165f91847cb44f745a8d5539666b24579
Mitch Daniels <mitch.daniels@viget.com>
2017-12-14 09:44:39 -0500

    update menu

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def mainMenu():
 	menu = {'1': 'Select a different room',
 			'2': 'Get User information',
 			'3': 'Most Popular Emoji',
-			'4': 'Exit'}
+			'4': 'Print current room and DataFrame',
+			'5': 'Exit'}
 
 	while True:
 		options = menu.keys()
@@ -17,25 +18,78 @@ def mainMenu():
 		selection = raw_input("Please select: ")
 
 		if selection == '1':
-			selectExportFolder()
+			selectRoom()
+			mainMenu()
+
 		elif selection == '2':
-			df = selectRoom()
+			selectRoom()
 			getUserInfo(df)
+
 		elif selection == '3':
 			print 'Most Popular Emoji'
+
 		elif selection == '4':
-			break
+			print room, df	
+
+		elif selection == '5':
+			sys.exit(0)
+
 		else:
 			print 'Unknown'
 
-def getUserInfo(df):
-	"""print list of most popular emojis associated with each user"""
+def setup():
+	global folder, room, df, userLookup
+	folder 	= 'Viget Slack export Aug 7 2017'
+	room 	= 'Crokinole'
+
+	df = pd.DataFrame()
+
+	users_json 	= open(folder + '/users.json', 'r').read()
+	userLookup = pd.read_json(users_json, orient = 'records')[['id','name']]
+
+	# import message files
+	path = folder + "/" + room + '/*.json'
+	for filename in glob.glob(path):
+		new_df = pd.read_json(filename, orient='records')
+		df = df.append(new_df, ignore_index = True)
+
+	pd.merge(df, userLookup, how='left', left_on='user', right_on='id')
+
+	return df
+
+def getRoomInfo():
+	"""print list of unique usernames and count of sent messages"""
+
+	'''
+	Between {startDate} and {endDate}, {activeUsers} users sent {totalMessages} messages
+	Most active users:
+		1. {userName} ({messageCount} messages)
+		2. {userName} ({messageCount} messages)
+		3. {userName} ({messageCount} messages)
+	'''
+	# startDate = min timestamp
+	# endDate = max timestamp
+	# activeUsers = df['user'].unique().count()
+	# totalMessage = df['text'].unique().count()
+
+	print df.groupby('user')['text'].count().sort_values(ascending=False)
+
+def getUserLookup():
+	"""create a two-column user lookup table (id, name)"""
+	global users
+	users = pd.read_json(users_json, orient = 'records')[['id','name']]
+
+def getUserInfo():
+	"""print user information"""
 	name = raw_input("Input username (first.last)")
 	for name in sorted(df.name.unique()):
 		print name
 		print getPopularEmoji(df[df.name == name]).sort_values(by='count', ascending=False)
 
-def getPopularEmoji(df):
+def compareMFUsage():
+	"""use uploaded .csv to compare male / female behavior"""
+
+def getPopularEmoji():
 	"""print list of most popular emojis associated with each user"""
 	d = {}
 	reactions = df.reactions.dropna()
@@ -49,10 +103,10 @@ def getPopularEmoji(df):
 	return pd.DataFrame(d.items(), columns = ['emoji', 'count'])
 
 def selectExportFolder():
-	# tests if it's a real exported slack folder
+	# Check if it's an exported Slack folder
 	while True:
 		try:
-			global folder, channels_json, users_json
+			global folder, userLookup
 			folder 			= raw_input("Slack export folder name: ")
 			channels_json 	= open(folder + '/channels.json', 'r').read()
 			users_json 		= open(folder + '/users.json', 'r').read()
@@ -60,7 +114,11 @@ def selectExportFolder():
 		except IOError:
 			print('Could not find correct files in that folder. Please try again.')
 
+	userLookup = pd.read_json(users_json, orient = 'records')[['id','name']]
+	selectRoom()
+
 def selectRoom():
+	global room, df
 	room = raw_input("Slack room name: ")
 
 	df = pd.DataFrame()
@@ -71,13 +129,9 @@ def selectRoom():
 		new_df = pd.read_json(filename, orient='records')
 		df = df.append(new_df, ignore_index = True)
 
-	users = pd.read_json(users_json)
-	user_lookup = users[['id','name']]
-
-	return df
+	print df
 
 if __name__ == '__main__':
-	# global variables: folder, channels_json, users_json
-
+	# global variables: folder, channels_json, users_json, df, room
 	selectExportFolder()
 	mainMenu()
